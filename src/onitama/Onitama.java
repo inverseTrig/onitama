b1106b913cdab5457bef6e0b0db3b5796f12debd
HeeChan Kang <heech.kang@gmail.com>
2017-12-21 14:39:52 -0600

    Tried to run the Onitama class with the new scanner.

--- src/onitama/Onitama.java
+++ src/onitama/Onitama.java
@@ -1,10 +1,17 @@
 package onitama;
 
+import java.io.IOException;
+
 public class Onitama {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
 		
+		Deck deck = new Deck();
+		deck.Reader();
+		
+		System.out.println(deck);
+		
+		System.out.println(deck.getDeck().size());
 	}
 
 }
