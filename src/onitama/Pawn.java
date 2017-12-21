05d806e57f90631544ca380e03bff146a64091c8
AJWalsh <walsh10101@gmail.com>
2017-12-21 00:52:15 -0600

    Worked on Pawn.

--- src/onitama/Pawn.java
+++ src/onitama/Pawn.java
@@ -1,5 +1,29 @@
 package onitama;
 
 public class Pawn {
+	
+	private boolean isRed;
+	private boolean isMaster;
+	
+	public Pawn(boolean isRed, boolean isMaster) {
+		this.isRed = isRed;
+		this.isMaster = isMaster;
+	}
 
+	public boolean isRed() {
+		return isRed;
+	}
+
+	public void setRed(boolean isRed) {
+		this.isRed = isRed;
+	}
+
+	public boolean isMaster() {
+		return isMaster;
+	}
+
+	public void setMaster(boolean isMaster) {
+		this.isMaster = isMaster;
+	}
+	
 }
