6d112b7bf7fe2d0aae11f169a493fdd3e00f2440
HeeChan Kang <heech.kang@gmail.com>
2017-12-21 14:39:17 -0600

    Added a file reader that reads the CSV file and creates Cards and puts them into the deck.

--- src/onitama/Deck.java
+++ src/onitama/Deck.java
@@ -1,6 +1,10 @@
 package onitama;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Deck {
 	
@@ -16,6 +20,49 @@ public class Deck {
 
 	public ArrayList<Card> getDeck() {
 		return deck;
+	}
+	
+	
+	public void Reader() throws IOException {
+		
+		File f = new File(System.getProperty("user.dir") + "/carddetail.csv");
+		
+		try {
+			Scanner scan = new Scanner(f);
+			scan.useDelimiter("\n");
+			
+			while(scan.hasNext()) {
+				String line = scan.next();
+				String cells[] = line.split(",");
+				
+				boolean isRed = true;
+				
+				int[][] moves = new int[5][2];
+				if (!cells[1].equalsIgnoreCase("red")) {
+					isRed = false;
+				}
+				
+				for (int i = 0; 2+(2*i) < cells.length; i++) {
+					moves[i][0] = Integer.parseInt(cells[2+(2*i)]);
+					moves[i][1] = Integer.parseInt(cells[3+(2*i)]);
+				}
+				
+				this.addCard(new Card(cells[0], isRed, moves));
+			}
+
+			scan.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file '" + f + "'");
+		}	
+	}
+	
+	public String toString() {
+		String s = "";
+		for (Card c : this.getDeck()) {
+			s += c.toString() + "\n";
+		}
+		return s;
 	}
 	
 }
