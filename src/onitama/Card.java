ae042188333c79eeba914db3960f92e5676540c0
HeeChan Kang <heech.kang@gmail.com>
2017-12-21 00:27:06 -0600

    Created Card constructors.

--- src/onitama/Card.java
+++ src/onitama/Card.java
@@ -1,5 +1,33 @@
 package onitama;
 
 public class Card {
+	
+	private int[][] moves = new int[10][2];
+	private boolean isRed;
+	
+	public Card(int[][] moves, boolean isRed) {
+		this.moves = moves;
+		this.isRed = isRed;
+	}
 
+
+	public int[][] getMoves() {
+		return moves;
+	}
+
+
+	public void setMoves(int[][] moves) {
+		this.moves = moves;
+	}
+
+
+	public boolean isRed() {
+		return isRed;
+	}
+
+
+	public void setRed(boolean isRed) {
+		this.isRed = isRed;
+	}
+	
 }
